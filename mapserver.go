31b87691e6568278f67e63bcb8b35d2eb2a0d0d4
Adam Crow <acrow@mail.dclabs.com.au>
2015-01-16 17:01:35 +1100

    replaced server with helloworld

--- mapserver.go
+++ mapserver.go
@@ -1,192 +1,20 @@
-package mapserver
+package mapserver 
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"sync"
-
-	"github.com/Sirupsen/logrus"
-	"github.com/crosbymichael/proxy"
-	"github.com/gorilla/mux"
 )
 
-type Server interface {
-	io.Closer
-	http.Handler
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World from Go in minimal Docker container")
 }
 
-type mapserver struct {
-	sync.Mutex
+func mapserver() {
+	http.HandleFunc("/", helloHandler)
 
-	r        *mux.Router
-	backends map[string]proxy.Proxy
-	logger   *logrus.Logger
-}
-
-func New(logger *logrus.Logger) Server {
-	r := mux.NewRouter()
-
-	s := &mapserver{
-		r:        r,
-		logger:   logger,
-		backends: make(map[string]proxy.Proxy),
-	}
-
-	r.HandleFunc("/", s.listBackends).Methods("GET")
-	r.HandleFunc("/{id:.*}", s.getBackend).Methods("GET")
-	r.HandleFunc("/{id:.*}", s.addBackend).Methods("POST")
-	r.HandleFunc("/{id:.*}", s.deleteBackend).Methods("DELETE")
-
-	return s
-}
-
-func (s *mapserver) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	s.r.ServeHTTP(w, r)
-}
-
-func (s *mapserver) Close() error {
-	s.Lock()
-	defer s.Unlock()
-
-	var err error
-	for id, p := range s.backends {
-		if nerr := p.Close(); nerr != nil {
-			s.logger.WithFields(logrus.Fields{
-				"error": err,
-				"id":    id,
-			}).Error("closing backend proxy")
-
-			err = nerr
-		}
-	}
-
-	return err
-}
-
-func (s *mapserver) listBackends(w http.ResponseWriter, r *http.Request) {
-	s.logger.Debug("listing backends")
-
-	out := []*proxy.Backend{}
-
-	s.Lock()
-	for _, p := range s.backends {
-		out = append(out, p.Backend())
-	}
-	s.Unlock()
-
-	s.marshal(w, out)
-}
-
-func (s *mapserver) getBackend(w http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)["id"]
-
-	s.logger.WithField("id", id).Debug("getting backend")
-
-	s.Lock()
-	p, exists := s.backends[id]
-	s.Unlock()
-
-	if !exists {
-		w.WriteHeader(http.StatusNotFound)
-
-		return
-	}
-
-	s.marshal(w, p.Backend())
-}
-
-func (s *mapserver) addBackend(w http.ResponseWriter, r *http.Request) {
-	var (
-		backend *proxy.Backend
-		id      = mux.Vars(r)["id"]
-	)
-
-	s.logger.WithField("id", id).Debug("adding new backend")
-
-	s.Lock()
-	_, exists := s.backends[id]
-	s.Unlock()
-
-	if exists {
-		http.Error(w, fmt.Sprintf("%s already exists", id), http.StatusConflict)
-
-		return
-	}
-
-	if err := json.NewDecoder(r.Body).Decode(&backend); err != nil {
-		s.logger.WithField("error", err).Error("decoding backend json")
-		http.Error(w, err.Error(), http.StatusBadRequest)
-
-		return
-	}
-	backend.Name = id
-
-	s.Lock()
-	defer s.Unlock()
-
-	proxy, err := proxy.New(backend)
+	fmt.Println("Started, serving at 8080")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		s.logger.WithField("error", err).Error("creating new proxy")
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-
-		return
-	}
-
-	s.backends[id] = proxy
-
-	if err := proxy.Start(); err != nil {
-		s.logger.WithFields(logrus.Fields{
-			"error": err,
-			"id":    id,
-		}).Error("starting new proxy")
-
-		delete(s.backends, id)
-
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-}
-
-func (s *mapserver) deleteBackend(w http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)["id"]
-
-	s.logger.WithField("id", id).Info("deleting backend")
-
-	s.Lock()
-	p, exists := s.backends[id]
-	s.Unlock()
-
-	if !exists {
-		w.WriteHeader(http.StatusNotFound)
-
-		return
-	}
-
-	if err := p.Close(); err != nil {
-		s.logger.WithFields(logrus.Fields{
-			"error": err,
-			"id":    id,
-		}).Error("closing backend proxy")
-
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-
-		// don't return here
-	}
-
-	s.Lock()
-	delete(s.backends, id)
-	s.Unlock()
-}
-
-func (s *mapserver) marshal(w http.ResponseWriter, v interface{}) {
-	w.Header().Set("content-type", "application/json")
-
-	if err := json.NewEncoder(w).Encode(v); err != nil {
-		s.logger.WithField("error", err).Error("marshal json")
+		panic("ListenAndServe: " + err.Error())
 	}
 }
